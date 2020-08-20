65ea704c56fca49fafb5510e2371bb80478aa83e
jasonpereira96 <jasonpereira074@gmail.com>
2020-08-20 22:51:07 +0530

    adding explanation

--- leetcode_542_side_flood_bfs_pattern.js
+++ leetcode_542_side_flood_bfs_pattern.js
@@ -9,6 +9,8 @@ Array.prototype.isEmpty = function () { return this.length === 0; };
 
 var updateMatrix = function (matrix) {
     const ROWS = matrix.length, COLS = matrix[0].length;
+
+    //make a copy of the matrix
     var grid = new Array(ROWS);
     for (var row = 0; row < ROWS; row++) {
         grid[row] = new Array(COLS).fill(-1);
@@ -16,19 +18,35 @@ var updateMatrix = function (matrix) {
 
     var queue = [];
     const directions = [[0, 1], [0, -1], [1, 0], [-1, 0]];
-
+    
+    //if the value in a cell is 0, then its distance from the nearest 0 is 0.
     for (var row = 0; row < grid.length; row++) {
         for (var col = 0; col < grid[0].length; col++) {
             if (matrix[row][col] === 0) {
                 grid[row][col] = 0;
+                //add all the cells with 0 to the queue.
+                //we start the BFS simultaneously from all the cells which have 0.
                 queue.push({
                     row, col
                 });
             }
         }
     }
+    
     var distance = 1;
 
+    /*
+    The main idea here is to expand radially outward from all the zeros.
+    Initally, we set the distance to 1.
+    As soon as we hit a 1, we set its distance to the value of distance
+
+    Initally, we start from all the cells whose distance from the nearest 0 is 0 (All the zeros)
+    Now, when we hit a 1, we mark its distance as 1.
+    Then we enqueue all the cells whose distance from the nearest zero is 1.
+    Now, when we hit a 1, we mark its distance as 2.
+
+    And so on...
+    */
     while (!queue.isEmpty()) {
         var tempQueue = [];
 
